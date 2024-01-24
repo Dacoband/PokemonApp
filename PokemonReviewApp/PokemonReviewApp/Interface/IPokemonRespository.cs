3027ce6c56acc227a46d4aa297f9d04b25121822
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-01-24 14:21:13 +0700

    1

---
+++ PokemonReviewApp/PokemonReviewApp/Interface/IPokemonRespository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interface
+{
+    public interface IPokemonRespository
+    {
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
+
+    }
+}
