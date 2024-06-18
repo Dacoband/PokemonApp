801e16525177f1dd5f01b88e50672c1156068bd2
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-06-18 16:30:09 +0700

    assembly

--- PokemonReviewApp/PokemonReviewApp/obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/PokemonReviewApp/obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b5cb87892fff60c6dbcd112f08a803e12fa98f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd31e603f6fb25024b796f46e876f5e59f183361")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
